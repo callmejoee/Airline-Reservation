dac9844467fc794c5855791afe291ea332510135
Ali <alirambo4444@gmail.com>
2023-05-20 18:14:08 +0300

    trial
    try github in ui

--- Airline Reservation/Ticket.Designer.cs
+++ Airline Reservation/Ticket.Designer.cs
@@ -28,137 +28,127 @@
         /// </summary>
         private void InitializeComponent()
         {
-            label1 = new Label();
-            comboBox2 = new ComboBox();
-            label7 = new Label();
-            label4 = new Label();
-            textBox3 = new TextBox();
-            label3 = new Label();
-            textBox2 = new TextBox();
-            label2 = new Label();
-            textBox1 = new TextBox();
-            SuspendLayout();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
             // 
             // label1
             // 
-            label1.AutoSize = true;
-            label1.BackColor = Color.Transparent;
-            label1.Font = new Font("Microsoft Sans Serif", 18F, FontStyle.Bold, GraphicsUnit.Point);
-            label1.ImageAlign = ContentAlignment.TopCenter;
-            label1.Location = new Point(280, 19);
-            label1.Name = "label1";
-            label1.Size = new Size(188, 29);
-            label1.TabIndex = 62;
-            label1.Text = "Ticket Booking";
-            label1.TextAlign = ContentAlignment.TopCenter;
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label1.Location = new System.Drawing.Point(320, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(226, 36);
+            this.label1.TabIndex = 62;
+            this.label1.Text = "Ticket Booking";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // comboBox2
             // 
-            comboBox2.FormattingEnabled = true;
-            comboBox2.Location = new Point(376, 270);
-            comboBox2.Name = "comboBox2";
-            comboBox2.Size = new Size(121, 23);
-            comboBox2.TabIndex = 81;
-            comboBox2.SelectedIndexChanged += comboBox2_SelectedIndexChanged;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(430, 360);
+            this.comboBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(138, 28);
+            this.comboBox2.TabIndex = 81;
             // 
             // label7
             // 
-            label7.AutoSize = true;
-            label7.BackColor = Color.Transparent;
-            label7.Font = new Font("Microsoft Sans Serif", 18F, FontStyle.Bold, GraphicsUnit.Point);
-            label7.ImageAlign = ContentAlignment.TopCenter;
-            label7.Location = new Point(237, 270);
-            label7.Name = "label7";
-            label7.Size = new Size(78, 29);
-            label7.TabIndex = 80;
-            label7.Text = "Class";
-            label7.TextAlign = ContentAlignment.TopCenter;
-            label7.Click += label7_Click;
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label7.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label7.Location = new System.Drawing.Point(271, 360);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(94, 36);
+            this.label7.TabIndex = 80;
+            this.label7.Text = "Class";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // label4
             // 
-            label4.AutoSize = true;
-            label4.BackColor = Color.Transparent;
-            label4.Font = new Font("Microsoft Sans Serif", 18F, FontStyle.Bold, GraphicsUnit.Point);
-            label4.ImageAlign = ContentAlignment.TopCenter;
-            label4.Location = new Point(237, 208);
-            label4.Name = "label4";
-            label4.Size = new Size(170, 29);
-            label4.TabIndex = 79;
-            label4.Text = "Passenger ID";
-            label4.TextAlign = ContentAlignment.TopCenter;
-            label4.Click += label4_Click;
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label4.Location = new System.Drawing.Point(271, 277);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(206, 36);
+            this.label4.TabIndex = 79;
+            this.label4.Text = "Passenger ID";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // textBox3
             // 
-            textBox3.Location = new Point(413, 214);
-            textBox3.Name = "textBox3";
-            textBox3.Size = new Size(132, 23);
-            textBox3.TabIndex = 78;
-            textBox3.TextChanged += textBox3_TextChanged;
+            this.textBox3.Location = new System.Drawing.Point(472, 285);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(150, 27);
+            this.textBox3.TabIndex = 78;
             // 
             // label3
             // 
-            label3.AutoSize = true;
-            label3.BackColor = Color.Transparent;
-            label3.Font = new Font("Microsoft Sans Serif", 18F, FontStyle.Bold, GraphicsUnit.Point);
-            label3.ImageAlign = ContentAlignment.TopCenter;
-            label3.Location = new Point(237, 151);
-            label3.Name = "label3";
-            label3.Size = new Size(111, 29);
-            label3.TabIndex = 77;
-            label3.Text = "Flight ID";
-            label3.TextAlign = ContentAlignment.TopCenter;
-            label3.Click += label3_Click;
-            // 
-            // textBox2
-            // 
-            textBox2.Location = new Point(431, 157);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(132, 23);
-            textBox2.TabIndex = 76;
-            textBox2.TextChanged += textBox2_TextChanged;
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label3.Location = new System.Drawing.Point(271, 201);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(135, 36);
+            this.label3.TabIndex = 77;
+            this.label3.Text = "Flight ID";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // label2
             // 
-            label2.AutoSize = true;
-            label2.BackColor = Color.Transparent;
-            label2.Font = new Font("Microsoft Sans Serif", 18F, FontStyle.Bold, GraphicsUnit.Point);
-            label2.ImageAlign = ContentAlignment.TopCenter;
-            label2.Location = new Point(237, 329);
-            label2.Name = "label2";
-            label2.Size = new Size(166, 29);
-            label2.TabIndex = 83;
-            label2.Text = "Seat Number";
-            label2.TextAlign = ContentAlignment.TopCenter;
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label2.Location = new System.Drawing.Point(271, 439);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(199, 36);
+            this.label2.TabIndex = 83;
+            this.label2.Text = "Seat Number";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(413, 335);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(132, 23);
-            textBox1.TabIndex = 82;
+            this.textBox1.Location = new System.Drawing.Point(472, 447);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(150, 27);
+            this.textBox1.TabIndex = 82;
             // 
             // Ticket
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
-            AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 711);
-            Controls.Add(label2);
-            Controls.Add(textBox1);
-            Controls.Add(comboBox2);
-            Controls.Add(label7);
-            Controls.Add(label4);
-            Controls.Add(textBox3);
-            Controls.Add(label3);
-            Controls.Add(textBox2);
-            Controls.Add(label1);
-            FormBorderStyle = FormBorderStyle.None;
-            Name = "Ticket";
-            StartPosition = FormStartPosition.CenterScreen;
-            Text = "Class";
-            ResumeLayout(false);
-            PerformLayout();
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(914, 948);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Ticket";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Class";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
@@ -169,7 +159,6 @@
         private Label label4;
         private TextBox textBox3;
         private Label label3;
-        private TextBox textBox2;
         private Label label2;
         private TextBox textBox1;
     }

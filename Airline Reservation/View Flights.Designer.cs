dab9f049066a1e21c0a254abc2e4419a8e203d2a
Youssef Karam <youssefkaramedu@gmail.com>
2023-05-21 20:51:51 +0300

    Add files

---
+++ Airline Reservation/View Flights.Designer.cs
@@ -0,0 +1,309 @@
+﻿namespace Airline_Reservation
+{
+    partial class View_Flights
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(View_Flights));
+            dateTimePicker2 = new DateTimePicker();
+            dateTimePicker1 = new DateTimePicker();
+            label8 = new Label();
+            textBox7 = new TextBox();
+            label7 = new Label();
+            label6 = new Label();
+            label5 = new Label();
+            textBox4 = new TextBox();
+            label4 = new Label();
+            textBox3 = new TextBox();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            label1 = new Label();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            dataGridView1 = new DataGridView();
+            label3 = new Label();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // dateTimePicker2
+            // 
+            dateTimePicker2.Location = new Point(550, 136);
+            dateTimePicker2.Name = "dateTimePicker2";
+            dateTimePicker2.Size = new Size(200, 23);
+            dateTimePicker2.TabIndex = 49;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(165, 137);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(200, 23);
+            dateTimePicker1.TabIndex = 48;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.BackColor = Color.Transparent;
+            label8.Font = new Font("Microsoft Sans Serif", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label8.ImageAlign = ContentAlignment.TopCenter;
+            label8.Location = new Point(12, 304);
+            label8.Name = "label8";
+            label8.Size = new Size(193, 29);
+            label8.TabIndex = 47;
+            label8.Text = "Available Seats";
+            label8.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(248, 304);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(132, 23);
+            textBox7.TabIndex = 46;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.BackColor = Color.Transparent;
+            label7.Font = new Font("Microsoft Sans Serif", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label7.ImageAlign = ContentAlignment.TopCenter;
+            label7.Location = new Point(30, 131);
+            label7.Name = "label7";
+            label7.Size = new Size(129, 29);
+            label7.TabIndex = 45;
+            label7.Text = "Departure";
+            label7.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.BackColor = Color.Transparent;
+            label6.Font = new Font("Microsoft Sans Serif", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label6.ImageAlign = ContentAlignment.TopCenter;
+            label6.Location = new Point(439, 132);
+            label6.Name = "label6";
+            label6.Size = new Size(87, 29);
+            label6.TabIndex = 44;
+            label6.Text = "Arrival";
+            label6.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.BackColor = Color.Transparent;
+            label5.Font = new Font("Microsoft Sans Serif", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label5.ImageAlign = ContentAlignment.TopCenter;
+            label5.Location = new Point(383, 216);
+            label5.Name = "label5";
+            label5.Size = new Size(144, 29);
+            label5.TabIndex = 42;
+            label5.Text = "Destination";
+            label5.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(537, 222);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(132, 23);
+            textBox4.TabIndex = 41;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = Color.Transparent;
+            label4.Font = new Font("Microsoft Sans Serif", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.ImageAlign = ContentAlignment.TopCenter;
+            label4.Location = new Point(30, 216);
+            label4.Name = "label4";
+            label4.Size = new Size(96, 29);
+            label4.TabIndex = 40;
+            label4.Text = "Source";
+            label4.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(185, 222);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(132, 23);
+            textBox3.TabIndex = 39;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.Transparent;
+            label2.Font = new Font("Microsoft Sans Serif", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.ImageAlign = ContentAlignment.TopCenter;
+            label2.Location = new Point(31, 70);
+            label2.Name = "label2";
+            label2.Size = new Size(111, 29);
+            label2.TabIndex = 37;
+            label2.Text = "Flight ID";
+            label2.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(170, 76);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(132, 23);
+            textBox1.TabIndex = 36;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Microsoft Sans Serif", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.ImageAlign = ContentAlignment.TopCenter;
+            label1.Location = new Point(316, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(156, 29);
+            label1.TabIndex = 35;
+            label1.Text = "View Flights";
+            label1.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            button2.Location = new Point(165, 393);
+            button2.Name = "button2";
+            button2.Size = new Size(132, 42);
+            button2.TabIndex = 50;
+            button2.Text = "Update";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            button3.Location = new Point(329, 393);
+            button3.Name = "button3";
+            button3.Size = new Size(132, 42);
+            button3.TabIndex = 51;
+            button3.Text = "Delete";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            button4.Location = new Point(500, 393);
+            button4.Name = "button4";
+            button4.Size = new Size(132, 42);
+            button4.TabIndex = 52;
+            button4.Text = "Back";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(30, 453);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(823, 274);
+            dataGridView1.TabIndex = 53;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = Color.Transparent;
+            label3.Font = new Font("Microsoft Sans Serif", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.ImageAlign = ContentAlignment.TopCenter;
+            label3.Location = new Point(848, 9);
+            label3.Name = "label3";
+            label3.Size = new Size(31, 29);
+            label3.TabIndex = 54;
+            label3.Text = "X";
+            label3.TextAlign = ContentAlignment.TopCenter;
+            label3.Click += label3_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(794, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(48, 39);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 55;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            // 
+            // View_Flights
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(891, 739);
+            Controls.Add(pictureBox1);
+            Controls.Add(label3);
+            Controls.Add(dataGridView1);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(dateTimePicker2);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(label8);
+            Controls.Add(textBox7);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(textBox4);
+            Controls.Add(label4);
+            Controls.Add(textBox3);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "View_Flights";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "View_Flights";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DateTimePicker dateTimePicker2;
+        private DateTimePicker dateTimePicker1;
+        private Label label8;
+        private TextBox textBox7;
+        private Label label7;
+        private Label label6;
+        private Label label5;
+        private TextBox textBox4;
+        private Label label4;
+        private TextBox textBox3;
+        private Label label2;
+        private TextBox textBox1;
+        private Label label1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private DataGridView dataGridView1;
+        private Label label3;
+        private PictureBox pictureBox1;
+    }
+}

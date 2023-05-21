fb227d19f587e1cf7326915ca84a0cc6448acb06
abdulrhman18 <abdomaldenii@gmail.com>
2023-05-21 17:09:28 +0300

    add craft

--- Airline Reservation/addAircraft.Designer.cs
+++ Airline Reservation/addAircraft.Designer.cs
@@ -37,50 +37,59 @@
             airline = new TextBox();
             button1 = new Button();
             label4 = new Label();
+            panel1 = new Panel();
+            label6 = new Label();
+            label7 = new Label();
+            panel1.SuspendLayout();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
             label1.BackColor = Color.Transparent;
-            label1.Font = new Font("Microsoft Sans Serif", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Font = new Font("Perpetua Titling MT", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.ForeColor = Color.Red;
             label1.ImageAlign = ContentAlignment.TopCenter;
-            label1.Location = new Point(387, 9);
+            label1.Location = new Point(309, 47);
             label1.Name = "label1";
-            label1.Size = new Size(148, 29);
+            label1.Size = new Size(266, 36);
             label1.TabIndex = 3;
             label1.Text = "Add Aircraft";
             label1.TextAlign = ContentAlignment.TopCenter;
             // 
             // planeID
             // 
-            planeID.Location = new Point(163, 69);
+            planeID.Location = new Point(444, 162);
+            planeID.Margin = new Padding(3, 4, 3, 4);
             planeID.Name = "planeID";
-            planeID.Size = new Size(132, 23);
+            planeID.Size = new Size(150, 27);
             planeID.TabIndex = 7;
             // 
             // label2
             // 
             label2.AutoSize = true;
             label2.BackColor = Color.Transparent;
-            label2.Font = new Font("Microsoft Sans Serif", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Font = new Font("Microsoft Sans Serif", 16.2F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.ForeColor = Color.Red;
             label2.ImageAlign = ContentAlignment.TopCenter;
-            label2.Location = new Point(9, 63);
+            label2.Location = new Point(248, 155);
             label2.Name = "label2";
-            label2.Size = new Size(112, 29);
+            label2.Size = new Size(122, 32);
             label2.TabIndex = 8;
             label2.Text = "Plane ID";
             label2.TextAlign = ContentAlignment.TopCenter;
+            label2.Click += label2_Click;
             // 
             // label3
             // 
             label3.AutoSize = true;
             label3.BackColor = Color.Transparent;
-            label3.Font = new Font("Microsoft Sans Serif", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Font = new Font("Microsoft Sans Serif", 16.2F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.ForeColor = Color.Red;
             label3.ImageAlign = ContentAlignment.TopCenter;
-            label3.Location = new Point(9, 140);
+            label3.Location = new Point(248, 224);
             label3.Name = "label3";
-            label3.Size = new Size(86, 29);
+            label3.Size = new Size(92, 32);
             label3.TabIndex = 10;
             label3.Text = "Model";
             label3.TextAlign = ContentAlignment.TopCenter;
@@ -88,20 +97,22 @@
             // 
             // model
             // 
-            model.Location = new Point(163, 146);
+            model.Location = new Point(444, 227);
+            model.Margin = new Padding(3, 4, 3, 4);
             model.Name = "model";
-            model.Size = new Size(132, 23);
+            model.Size = new Size(150, 27);
             model.TabIndex = 9;
             // 
             // label5
             // 
             label5.AutoSize = true;
             label5.BackColor = Color.Transparent;
-            label5.Font = new Font("Microsoft Sans Serif", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label5.Font = new Font("Microsoft Sans Serif", 16.2F, FontStyle.Regular, GraphicsUnit.Point);
+            label5.ForeColor = Color.Red;
             label5.ImageAlign = ContentAlignment.TopCenter;
-            label5.Location = new Point(9, 213);
+            label5.Location = new Point(248, 293);
             label5.Name = "label5";
-            label5.Size = new Size(88, 29);
+            label5.Size = new Size(95, 32);
             label5.TabIndex = 14;
             label5.Text = "Airline";
             label5.TextAlign = ContentAlignment.TopCenter;
@@ -109,20 +120,23 @@
             // 
             // airline
             // 
-            airline.Location = new Point(163, 213);
+            airline.Location = new Point(444, 292);
+            airline.Margin = new Padding(3, 4, 3, 4);
             airline.Name = "airline";
-            airline.Size = new Size(132, 23);
+            airline.Size = new Size(150, 27);
             airline.TabIndex = 13;
             // 
             // button1
             // 
-            button1.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
-            button1.Location = new Point(325, 431);
+            button1.BackColor = SystemColors.Highlight;
+            button1.Font = new Font("Comic Sans MS", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.Location = new Point(360, 415);
+            button1.Margin = new Padding(3, 4, 3, 4);
             button1.Name = "button1";
-            button1.Size = new Size(132, 42);
+            button1.Size = new Size(151, 56);
             button1.TabIndex = 15;
             button1.Text = "Add";
-            button1.UseVisualStyleBackColor = true;
+            button1.UseVisualStyleBackColor = false;
             button1.Click += button1_Click;
             // 
             // label4
@@ -131,19 +145,58 @@
             label4.BackColor = Color.Transparent;
             label4.Font = new Font("Microsoft Sans Serif", 18F, FontStyle.Bold, GraphicsUnit.Point);
             label4.ImageAlign = ContentAlignment.TopCenter;
-            label4.Location = new Point(908, 9);
+            label4.Location = new Point(1038, 12);
             label4.Name = "label4";
-            label4.Size = new Size(31, 29);
+            label4.Size = new Size(37, 36);
             label4.TabIndex = 16;
             label4.Text = "X";
             label4.TextAlign = ContentAlignment.TopCenter;
             label4.Click += label4_Click;
             // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.Highlight;
+            panel1.Controls.Add(label7);
+            panel1.Controls.Add(label6);
+            panel1.Location = new Point(-5, -6);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(247, 546);
+            panel1.TabIndex = 17;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.BackColor = Color.Transparent;
+            label6.Font = new Font("Comic Sans MS", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label6.ForeColor = SystemColors.ButtonHighlight;
+            label6.ImageAlign = ContentAlignment.TopCenter;
+            label6.Location = new Point(17, 260);
+            label6.Name = "label6";
+            label6.Size = new Size(221, 29);
+            label6.TabIndex = 4;
+            label6.Text = "You can add Aircarft";
+            label6.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.BackColor = Color.Transparent;
+            label7.Font = new Font("Comic Sans MS", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label7.ForeColor = SystemColors.ButtonHighlight;
+            label7.ImageAlign = ContentAlignment.TopCenter;
+            label7.Location = new Point(55, 299);
+            label7.Name = "label7";
+            label7.Size = new Size(131, 29);
+            label7.TabIndex = 5;
+            label7.Text = "from there.";
+            label7.TextAlign = ContentAlignment.TopCenter;
+            // 
             // addAircraft
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(951, 533);
+            ClientSize = new Size(651, 507);
+            Controls.Add(panel1);
             Controls.Add(label4);
             Controls.Add(button1);
             Controls.Add(label5);
@@ -154,9 +207,12 @@
             Controls.Add(planeID);
             Controls.Add(label1);
             FormBorderStyle = FormBorderStyle.None;
+            Margin = new Padding(3, 4, 3, 4);
             Name = "addAircraft";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "addAircraft";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -172,5 +228,8 @@
         private TextBox airline;
         private Button button1;
         private Label label4;
+        private Panel panel1;
+        private Label label7;
+        private Label label6;
     }
 }
--- Airline Reservation/addAircraft.cs
+++ Airline Reservation/addAircraft.cs
@@ -63,5 +63,15 @@ namespace Airline_Reservation
                 }
             }
         }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }

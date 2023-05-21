30d4a1e6953f3e6d6c3f87af5a4415183be156ff
Youssef Karam <youssefkaramedu@gmail.com>
2023-05-21 19:08:36 +0300

    Add user home page

--- Airline Reservation/userHome.Designer.cs
+++ Airline Reservation/userHome.Designer.cs
@@ -28,20 +28,87 @@
         /// </summary>
         private void InitializeComponent()
         {
+            button6 = new Button();
+            button5 = new Button();
+            button3 = new Button();
+            label2 = new Label();
+            label1 = new Label();
             SuspendLayout();
             // 
-            // userAdmin
+            // button6
+            // 
+            button6.Location = new Point(497, 363);
+            button6.Name = "button6";
+            button6.Size = new Size(140, 23);
+            button6.TabIndex = 21;
+            button6.Text = "Logout";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(317, 363);
+            button5.Name = "button5";
+            button5.Size = new Size(140, 23);
+            button5.TabIndex = 20;
+            button5.Text = "Book a ticket";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(125, 363);
+            button3.Name = "button3";
+            button3.Size = new Size(170, 23);
+            button3.TabIndex = 19;
+            button3.Text = "View Flights";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(774, 9);
+            label2.Name = "label2";
+            label2.Size = new Size(14, 15);
+            label2.TabIndex = 18;
+            label2.Text = "X";
+            label2.Click += label2_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(359, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(79, 32);
+            label1.TabIndex = 17;
+            label1.Text = "Home";
+            // 
+            // userHome
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button3);
+            Controls.Add(label2);
+            Controls.Add(label1);
             FormBorderStyle = FormBorderStyle.None;
-            Name = "userAdmin";
+            Name = "userHome";
             StartPosition = FormStartPosition.CenterScreen;
-            Text = "userAdmin";
+            Text = "userHome";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button6;
+        private Button button5;
+        private Button button3;
+        private Label label2;
+        private Label label1;
     }
 }
--- Airline Reservation/userHome.cs
+++ Airline Reservation/userHome.cs
@@ -16,5 +16,31 @@ namespace Airline_Reservation
         {
             InitializeComponent();
         }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Login login = new Login();
+            login.Show();
+            this.Hide();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Ticket ticket = new Ticket();
+            ticket.Show();
+            this.Hide();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            View_Flights view_Flights = new View_Flights();
+            view_Flights.Show();
+            this.Hide();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+           
+        }
     }
 }
